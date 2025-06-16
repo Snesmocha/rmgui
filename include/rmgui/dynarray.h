a88e9496f5f2ed9acf5159323108e321abe67cc3
Snesmocha <60557323+Snesmocha@users.noreply.github.com>
2025-06-16 14:34:25 -0400

    Update dynarray.h

--- include/rmgui/dynarray.h
+++ include/rmgui/dynarray.h
@@ -6,26 +6,13 @@
 #include <string.h>
 #include <limits.h>
 
+#include "util.h"
+
 #ifdef _MSC_VER
 #include <intrin.h>
 #endif
 
 #define VECTOR_INITIAL_CAPACITY 8
-
-/* put this in some utilities header or something */
-#if defined(__GNUC__) || defined(__clang__)
-
-#define UNLIKELY(x) __builtin_expect(!!(x), 0)
-#define LIKELY(x) __builtin_expect(!!(x), 1)
-
-#else   /*MSVC is the only crappy compiler that doesnt support this*/
-
-#define UNLIKELY(x) (x)
-#define LIKELY(x) (x)
-
-#endif
-
-    
 
 typedef enum
 {
